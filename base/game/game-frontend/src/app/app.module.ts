c6ec5a2f6d0719695129c717a61dfa67f6711e93
Maxime Sorel <sorel.max@gmail.com>
2022-09-07 11:25:25 +0200

    NEW: ajout base
    - ajout du contenu du zip "base" pris sur Moodle

---
+++ base/game/game-frontend/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSelectModule} from "@angular/material/select";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {FormsModule} from "@angular/forms";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {RouterModule} from "@angular/router";
+import {InteractoModule, interactoTreeUndoProviders} from "interacto-angular";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        InteractoModule,
+        MatSelectModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatIconModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatListModule,
+        FormsModule,
+        MatCheckboxModule,
+        RouterModule
+    ],
+  providers: [interactoTreeUndoProviders(true)],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
