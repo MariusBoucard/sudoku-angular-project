eeef8ef2d936915d9853d83089fb5a6b7a674007
Marius Boucard Bocciarelli <marius.boucard--bocciarelli@insa-rennes.fr>
2022-11-24 23:57:37 +0100

    changed stategy where it bugs

--- game/game-backend/src/main/java/game/service/SavingService.java
+++ game/game-backend/src/main/java/game/service/SavingService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -53,7 +52,7 @@ public class SavingService {
         try {
             final String a = new ObjectMapper().writeValueAsString(gridMap);
             System.out.println(a);
-            Path path = Path.of(outputfile);
+            final Path path = Path.of(outputfile);
             Files.writeString(path, a);
 
 
