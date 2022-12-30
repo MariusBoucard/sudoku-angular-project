4ef8d4486b44e1a5c46c15a434a267f5ebfbfd9a
Maxime Sorel <sorel.max@gmail.com>
2022-12-30 12:58:22 +0100

    FIX: new component test created locally
    -j'avais créé un nouveau composant localement mais qui ne marche pas
    encore mais le projet ne veut pas compiler sans

---
+++ game/game-frontend/src/app/components/endgame-dialog/endgame-dialog.component.ts
@@ -0,0 +1,54 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {Player} from 'src/app/classes/player';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatDialogRef} from "@angular/material/dialog";
+import { GameService } from 'src/app/services/game.service';
+
+@Component({
+  selector: 'app-endgame-dialog',
+  templateUrl: './endgame-dialog.component.html',
+  styleUrls: ['./endgame-dialog.component.css']
+})
+/**
+ * This class has one instance for each grid
+ * In it we can find the players that played the grid
+ * and the score they did on it. Because it is a component
+ * we gotta do some display on it, and add some functions
+ */
+export class EndgameDialogComponent implements OnInit {
+  classement: Player[] = [];
+
+  constructor(public dialogRef: MatDialogRef<EndgameDialogComponent>,@Inject('gameServ')  public gameService : GameService) {
+  }
+
+  ngOnInit(): void {
+    this.gameService.currentGame.grid.classement.classement.forEach(element => {
+      this.addToClassement(element);
+
+    });
+
+  }
+
+
+  /**
+   *
+   * @param joueur is the player we want to add to the classement.
+   * At the end of the game only, we can check his score and insert it in the right place in the placement.
+   *
+   */
+  addToClassement(joueur: Player) {
+    this.classement.push(joueur);
+    this.classement.sort((p1, p2) => (p1.score < p2.score ? -1 : 1)); //pas optimal mais c'est déjà ça
+    if (this.classement.length >= 5){
+      this.classement = this.classement.slice(0,5);
+    }
+  }
+
+  getClassement(): Player[] {
+    return this.classement;
+  }
+
+
+  dataSource = new MatTableDataSource(this.getClassement());
+  displayedColumns: string[] = ['name', 'score'];
+}
