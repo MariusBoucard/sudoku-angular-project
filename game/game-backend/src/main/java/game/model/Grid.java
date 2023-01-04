aa6442348344b48c48dc903da79f2ab3f434e3dc
Marius Boucard Bocciarelli <marius.boucard--bocciarelli@insa-rennes.fr>
2023-01-04 13:03:51 +0100

    Ha oui c est vrai c est le score le plus faire qui est le mieux j suis un golmut

--- game/game-backend/src/main/java/game/model/Grid.java
+++ game/game-backend/src/main/java/game/model/Grid.java
@@ -56,7 +56,8 @@ public class Grid implements Serializable {
      */
     public void addScore(final Player player) {
             final List<Player> classm = classement.getClassement();
-                if(classement.getClassement().stream().filter(a -> a.name.equals(player.name) && a.score >= player.score).count() == 0) {
+
+                if(classement.getClassement().stream().filter(a -> a.name.equals(player.name) && a.score <= player.score).count() == 0) {
                     final List<Player> list  = classement.getClassement().stream().filter(a -> a.name.equals(player.name)).collect(Collectors.toList());
                     list.forEach(elem ->  classm.remove(elem));
                     classement.setClassement((ArrayList<Player>) classm);
